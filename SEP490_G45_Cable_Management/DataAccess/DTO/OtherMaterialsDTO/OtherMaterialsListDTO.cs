ddb647548e85c719fc24d0a0e4b7c212da5cfc04
Duc8101 <vegitobasicinstict1810@gmail.com>
2023-10-23 13:08:38 +0700

    .

--- SEP490_G45_Cable_Management/DataAccess/DTO/OtherMaterialsDTO/OtherMaterialsListDTO.cs
+++ SEP490_G45_Cable_Management/DataAccess/DTO/OtherMaterialsDTO/OtherMaterialsListDTO.cs
@@ -14,7 +14,7 @@ namespace DataAccess.DTO.OtherMaterialsDTO
         public string? Code { get; set; }
         //public string? SupplierName { get; set; }
         // public string SupplierName { get; set; } = null!;
-        public string? Status { get; set; }
+        public string Status { get; set; } = null!;
         public int? WarehouseId { get; set; }
         public string? WarehouseName { get; set; }
         public int OtherMaterialsCategoryId { get; set; }
