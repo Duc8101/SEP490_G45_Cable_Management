cb4662abc2fb276736aadaacd564ae739474b1cb
Duc8101 <vegitobasicinstict1810@gmail.com>
2024-06-29 22:10:23 +0700

    .

--- SEP490_G45_Cable_Management/UnitTests/obj/Debug/net6.0/UnitTests.AssemblyInfo.cs
+++ SEP490_G45_Cable_Management/UnitTests/obj/Debug/net6.0/UnitTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+baff604f941cedef5f20e77ef374f0da710bc06a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c79679644bb6b11f69debf1da8ba701791cd587f")]
 [assembly: System.Reflection.AssemblyProductAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
