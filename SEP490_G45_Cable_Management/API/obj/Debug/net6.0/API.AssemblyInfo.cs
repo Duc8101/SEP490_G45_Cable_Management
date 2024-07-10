feb0e9b7645b1eba6d71a1f1f4b8b8dd2a904066
Duc8101 <vegitobasicinstict1810@gmail.com>
2024-07-10 15:49:27 +0700

    .

--- SEP490_G45_Cable_Management/API/obj/Debug/net6.0/API.AssemblyInfo.cs
+++ SEP490_G45_Cable_Management/API/obj/Debug/net6.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d1c75b920e40681d7c35b4dfa80e23446ae89ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29ab9c323bc6c72f6022a8344f4934d80b42d914")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- SEP490_G45_Cable_Management/DataAccess/Configuration/ConfigData.cs
+++ SEP490_G45_Cable_Management/DataAccess/Configuration/ConfigData.cs
@@ -4,13 +4,40 @@ namespace DataAccess.Configuration
 {
     public class ConfigData
     {
-        public static string SqlConnection { get; } = getConfigValue("ConnectionStrings:DefaultConnection");
-        public static string JwtKey { get; } = getConfigValue("Jwt:Key");
-        public static string JwtIssuer { get; } = getConfigValue("Jwt:Issuer");
-        public static string JwtAudience { get; } = getConfigValue("Jwt:Audience");
-        public static string MailHost { get; } = getConfigValue("MailAddress:Host");
-        public static string MailUser { get; } = getConfigValue("MailAddress:Username");
-        public static string MailPassword { get; } = getConfigValue("MailAddress:Password");
+        public static string SqlConnection
+        {
+            get => getConfigValue("ConnectionStrings:DefaultConnection");
+        }
+
+        public static string JwtKey
+        {
+            get => getConfigValue("Jwt:Key");
+        }
+
+        public static string JwtIssuer
+        {
+            get => getConfigValue("Jwt:Issuer");
+        }
+
+        public static string JwtAudience
+        {
+            get => getConfigValue("Jwt:Audience");
+        }
+
+        public static string MailHost
+        {
+            get => getConfigValue("MailAddress:Host");
+        }
+
+        public static string MailUser
+        {
+            get => getConfigValue("MailAddress:Username");
+        }
+
+        public static string MailPassword
+        {
+            get => getConfigValue("MailAddress:Password");
+        }
 
         private static string getConfigValue(string configPath)
         {
